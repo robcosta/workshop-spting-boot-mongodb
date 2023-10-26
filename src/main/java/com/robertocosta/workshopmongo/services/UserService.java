8025eb146572dba364539a5062bd2d5176fe3bfa
Roberto Costa <robertocosta66@gmail.com>
2023-10-26 15:57:26 -0300

    User insert with Post

--- src/main/java/com/robertocosta/workshopmongo/services/UserService.java
+++ src/main/java/com/robertocosta/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.robertocosta.workshopmongo.domain.User;
+import com.robertocosta.workshopmongo.dto.UserDTO;
 import com.robertocosta.workshopmongo.repository.UserRepository;
 import com.robertocosta.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("User not found (id: "+id+")"));
+	}
+	
+	public User insert(User obj) {
+		return repo.save(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
