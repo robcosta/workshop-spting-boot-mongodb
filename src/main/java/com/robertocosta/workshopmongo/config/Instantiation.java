fe666df0cecb5b60fc0610d0eb1b4e40e6928ed0
Roberto Costa <robertocosta66@gmail.com>
2023-10-27 10:35:17 -0300

    Post entity and PostRepository

--- src/main/java/com/robertocosta/workshopmongo/config/Instantiation.java
+++ src/main/java/com/robertocosta/workshopmongo/config/Instantiation.java
@@ -1,12 +1,18 @@
 package com.robertocosta.workshopmongo.config;
 
+import java.text.Format;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import com.robertocosta.workshopmongo.domain.Post;
 import com.robertocosta.workshopmongo.domain.User;
+import com.robertocosta.workshopmongo.repository.PostRepository;
 import com.robertocosta.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -14,17 +20,26 @@ public class Instantiation implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+				
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		
+		Post p1 = new Post(null, Instant.parse("2018-06-20T19:53:07Z"),"Patiu viagem","Vou viajar para São Paulo. Abraços!", maria);
+		Post p2 = new Post(null, Instant.parse("2018-06-25T19:53:07Z"),"Bom dia","Acordei feliz hoje", maria);
+			
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(p1,p2));
 	}
 
 }
